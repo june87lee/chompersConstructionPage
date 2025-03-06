9a786bacabc4b62835da01db0da47e12120296c2
June <Lee@JL-Macbook-Pro.local>
2025-03-06 11:32:33 -0800

    FIX: tab icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,15 +29,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Chomper's Construction`,
+        short_name: `Chomper`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#1b2229`, // chompBlack
+        theme_color: `#fcb922`, // chompYellowLight
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/logoIcon_ver001.png`,
       },
     },
   ],
