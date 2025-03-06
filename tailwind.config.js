99fb15619bffd2f95c1e4f63fc30c53839b59bdc
June <Lee@JL-Macbook-Pro.local>
2025-03-06 11:04:50 -0800

    ADD: styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        chompBlack: "#1b2229",
+        chompYellowLight: "#fcb922",
+        chompYellowDark: "#e79917",
+        chompBlueLight: "#2d5377",
+        chompBlueDark: "#153851",
+        chompWhite: "#fdfefd",
+      },
+    },
   },
   plugins: [],
 }
